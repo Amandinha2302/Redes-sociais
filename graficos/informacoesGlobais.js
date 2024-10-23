2590d5408e9e115299a0fac832bb6894d7c5f05d
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-10-23 07:51:06 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,9 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
 
+async function visualizarInformacoesGlobais() {
+  const res = awaif ferch(url)
+  const dados = await res.json()
+  console.log(dados);
+}
+
+visualizarInformacoesGlobais();
